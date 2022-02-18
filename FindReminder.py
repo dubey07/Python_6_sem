8123d5c12134c26a3d7a12890f1c520edb7c2361
Aaditya dubey <adi.dubey01@gmail.com>
2022-02-18 19:06:38 +0530

    Update FindReminder.py

--- FindReminder.py
+++ FindReminder.py
@@ -1,6 +1,4 @@
-a = int(input())
-tests = []
-for i in range(a):
-    tests.append([int(i) for i in input().split()])
-for i in tests:
-    print(i[0]%i[1]
+T = int(input())
+for i in range(T):
+    A,B = map(int,input().split())
+    print(A%B)
