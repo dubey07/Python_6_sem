5473dfabba0e18d8cc98a21c587ff42ec9de15fa
Aaditya dubey <adi.dubey01@gmail.com>
2022-04-30 01:48:41 +0530

    Create List_tuple_dictionary.py

---
+++ List_tuple_dictionary.py
@@ -0,0 +1,34 @@
+# list
+
+# 1
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# 2
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+# Tuple
+
+# 1
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# 2
+tuple1 = (0, 1, 2, 3)
+tuple2 = ('python', 'geek')
+
+# dictionary
+
+#1
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# 2
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
