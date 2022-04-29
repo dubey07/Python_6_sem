84a15bf8852d5669c9f584776e8e535939e89520
Aaditya dubey <adi.dubey01@gmail.com>
2022-04-30 02:08:23 +0530

    Create polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,11 @@
+p = 55
+q = 77
+r = 9.5
+g1 = "Guru"
+g2 = "99!"
+print("the sum of two numbers",p + q)
+print("the data type of result is",type(p + q))
+print("The sum of two numbers",q + r)
+print("the data type of result is", type (q + r))
+print("The concatenated string is", g1 + g2)
+print("The data type of two strings",type(g1 + g2))
